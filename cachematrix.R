8c5e1f12fe4dfb185757d39ebcd39671cb18be4e
Pegasusxu <xushizhe@gmail.com>
2015-08-21 19:49:57 -0700

    Final Version 	modified:   cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -23,16 +23,3 @@ cacheSolve <- function(z, ...) {
         z$setinv(t)
         t
 }
-
-test = function(mat){
-        temp = makeCacheMatrix(mat)
-        start.time = Sys.time()
-        cacheSolve(temp)
-        dur = Sys.time() - start.time
-        print(dur)
-        
-        start.time = Sys.time()
-        cacheSolve(temp)
-        dur = Sys.time() - start.time
-        print(dur)
-}
